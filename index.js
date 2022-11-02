71e6324d68246146d40488ddb7770e273f4dfa4a
Donovan <donovangriffin04@gmail.com>
2022-11-02 15:41:19 -0700

    added comments to explain what i was doing

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//select the tags to manipulate
 const p = document.querySelector('p')
 const button = document.querySelector('button')
 let html = document.querySelector('html')
+//add an event listener on the click
 button.addEventListener('click', function(){
-  let random1 = Math.floor(Math.random() * 255);
-  let random2 = Math.floor(Math.random() * 255);
-  let random3 = Math.floor(Math.random() * 255);
+  // select 3 different random numbers
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
 
-
-  html.style.backgroundColor = (`rgb(${random1},${random2}, ${random3})`) 
-  console.log('vlicled')
+//change the color of the html page
+  html.style.backgroundColor = (`rgb(${red},${green}, ${blue})`) 
+  
 
 })
