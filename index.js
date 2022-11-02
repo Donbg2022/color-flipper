2efbc7e6e851caca970bc54903ee20e47070590c
Donovan <donovangriffin04@gmail.com>
2022-11-02 16:37:37 -0700

    added a "go back" feature which allows user to go back to the last color

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const p = document.querySelector('p')
 const button = document.querySelector('button')
 let html = document.querySelector('html')
+let previousColours = []
+
 //add an event listener on the click
 button.addEventListener('click', function(){
   // select 3 different random numbers
@@ -16,6 +18,26 @@ button.addEventListener('click', function(){
   //changes P text to what the rgb value is 
   p.innerText = `rgb(${red},${green},${blue})`
 
+  previousColours.push(`rgb(${red},${green},${blue})`)
   
+  // if the array gets to 10 it auto deletes the first item so that my slow laptop doesnt freeze lol
+  if (previousColours.length > 9){
+    previousColours.shift()
+  }
+  // makes clicked 0 so it doesnt accumulate and go back too many times in array
+  clicked = 0
+})
+
+const lastColorbtn = document.querySelector('#lastcolor')
+let clicked = 0
+
+// click event listener to enable cycling backwards
+lastColorbtn.addEventListener('click', function(e){
+  clicked++
+//finds the last item of array by taking the length and subtracting one and the uses the clicked variable to let it go back continuously 
+  html.style.backgroundColor = previousColours[previousColours.length - (1 + clicked)]  
+  //changes p to the rgb value
+  p.innerText = previousColours[previousColours.length - (1 + clicked)]
+ 
 
 })
